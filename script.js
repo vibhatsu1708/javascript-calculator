525ea3fcf1a8c5d9c2944f1479f036d687fac028
Vedant Mistry <mistryvedant17@gmail.com>
2022-02-28 15:34:50 +0530

    completed code of the calculator project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const numbers_display = document.querySelector('numbers_display');
-const button = document.querySelectorAll('button');
+const display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
 
 function add (num1, num2) {
     return num1 + num2;
@@ -40,11 +40,26 @@ function operate (operator) {
 function buttonClicked () {
     
 }
+buttons.map(
+    button => button.addEventListener (
+        'click', (e) => {
+            switch (e.target.innerText) {
+                case 'C' :
+                    display.innerText = '';
+                    break;
 
-function main () {
-    button.addEventListener (
-        'click', function () {
+                case '=' :
+                    try {
+                        display.innerText = eval(display.innerText);
+                        break;
+                    }
+                    catch {
+                        display.innerText = 'ERROR';
+                    }
 
+                default :
+                    display.innerText += e.target.innerText;
+            }
         }
-    ) 
-}
+    )
+)
